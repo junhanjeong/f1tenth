feat: fix start points and reward scaling

gym/f110_gym/envs/f110_env.py
@@ -683,10 +683,10 @@ class F110Env(gym.Env, utils.EzPickle):
         goal = np.array(self.goals[next_idx])
         pos  = np.array([obs['poses_x'][0], obs['poses_y'][0]])
         curr_dist = np.linalg.norm(goal - pos)
-        # if hasattr(self, 'prev_goal_dist') and self.prev_goal_dist is not None:
-        #     # 거리가 줄어들면 양수, 늘어나면 음수
-        #     reward += (self.prev_goal_dist - curr_dist) * 1.0
-        #     reward = float(np.clip(reward, -0.03, 0.03))
+        if hasattr(self, 'prev_goal_dist') and self.prev_goal_dist is not None:
+            # 거리가 줄어들면 양수, 늘어나면 음수
+            reward += (self.prev_goal_dist - curr_dist) * 5.0
+            reward = float(np.clip(reward, -0.1, 0.1))
         self.prev_goal_dist = curr_dist
         
         # # 2. speed reward: 전진 속도 장려

pkg/src/pkg/double_dqn.py
@@ -12,7 +12,7 @@ from collections import deque
 from tqdm import trange
 
 # Hyperparameters
-LR = 3e-4
+LR = 0.00042
 GAMMA = 0.98
 BUFFER_SIZE = 50000
 BATCH_SIZE = 32
@@ -169,7 +169,6 @@ def main():
     epsilon = 1.0
 
     for episode in range(10000):
-        epsilon = max(0.1, epsilon * 0.99994)
         obs, _, _, _ = env.reset(poses=np.array([[0., 0., np.radians(270)]]))
         lidar0 = preprocess_lidar(obs['scans'][0])
         prev = lidar0.copy()
@@ -179,6 +178,7 @@ def main():
         laptime = 0.0
 
         while not done:
+            epsilon = max(0.1, epsilon * 0.99994)
             action = q_net.act(state, epsilon)
             steer = STEER_VALUES[action]
             action_np = np.array([[steer, SPEED]])